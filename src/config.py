3dda83b58d04f9f6f0f15e8cb6dcc3660a531bf0
Alex Brosseau <brosseau.alex@outlook.com>
2025-10-03 19:13:40 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -9,12 +9,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = int(os.getenv("DB_PORT"))
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST", "mysql")
+DB_PORT = int(os.getenv("DB_PORT", "3306"))
+DB_NAME = os.getenv("DB_NAME", "labo03_db")
+# accept either DB_USERNAME or DB_USER
+DB_USER = os.getenv("DB_USERNAME") or os.getenv("DB_USER") or "labo03"
+DB_PASS = os.getenv("DB_PASSWORD") or os.getenv("DB_PASS") or "labo03"
 
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = int(os.getenv("REDIS_PORT"))
-REDIS_DB = int(os.getenv("REDIS_DB"))
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_DB   = int(os.getenv("REDIS_DB", "0"))
+
